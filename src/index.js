c32d0b85f9b95567fcd5e068361e5d37f50fd5a7
gabino mendez <gabino_mendez@hotmail.com.ar>
2022-05-29 20:50:56 -0300

    agregando navBar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+//traigo la libreria REACT
 import React from 'react';
+//traigo la libreria de reactDOM
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// agarrar una app que ya existia en REACT
 
+import "./estilos.css";
+import App from './App';
+// const App = ()=> "hola mundo con jsx y arrow function"
+//mostrar la app en el dom
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
